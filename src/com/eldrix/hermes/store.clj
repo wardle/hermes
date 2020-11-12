3bcefcdbc397d0d3b720d49063e8290bd76c0d3c
Mark Wardle <mark@wardle.org>
2020-11-12 13:18:06 +0000

    Add reverse mapping to SNOMED CT.
    Add index to facilitate mapping from an external code into SNOMED-CT.

--- src/com/eldrix/hermes/store.clj
+++ src/com/eldrix/hermes/store.clj
@@ -5,13 +5,20 @@
   (:import [java.io FileNotFoundException Closeable]
            (org.mapdb Serializer)
            (org.mapdb.serializer SerializerArrayTuple)
-           (java.util NavigableSet)))
+           (java.util NavigableSet)
+           (com.eldrix.hermes.snomed Description)))
 
 (set! *warn-on-reflection* true)
 
-(deftype MapDBStore [db concepts descriptions relationships refsets cache indexes]
+(deftype MapDBStore [^org.mapdb.DB db
+                     ^org.mapdb.BTreeMap concepts
+                     ^org.mapdb.BTreeMap descriptions
+                     ^org.mapdb.BTreeMap relationships
+                     ^org.mapdb.BTreeMap refsets
+                     ^org.mapdb.BTreeMap cache
+                     indexes]
   Closeable
-  (close [_] db))
+  (close [_] (.close db)))
 
 (defn- ^org.mapdb.DB open-database
   "Open a file-based key-value database from the file specified, optionally read only. Use in a with-open
@@ -48,9 +55,8 @@
 
 (defn- write-object
   "Write an object into a bucket.
-  Correctly handling atomicity and only writes the object if the
-  effectiveTime of the entity is newer than the version that already exists in
-  that bucket."
+  Correctly handles atomicity and only writes the object if the effectiveTime of
+  the entity is newer than the version that already exists in that bucket."
   ([^org.mapdb.MapExtra bucket o] (write-object bucket o 0))
   ([^org.mapdb.MapExtra bucket o attempt]
    (when-not (.putIfAbsentBoolean bucket (:id o) o)
@@ -69,7 +75,7 @@
 (defn- write-index-entry
   "Write an index entry `k` into the `bucket` specified.
   Adds a key to the set, if active.
-  Removes a key to the set if not active."
+  Removes a key from the set if not active."
   [^java.util.NavigableSet bucket k active?]
   (if active?
     (.add bucket k)
@@ -77,14 +83,15 @@
 
 (defn build-description-index
   [^MapDBStore store]
-  (let [bucket ^org.mapdb.BTreeMap (:concept-descriptions (.indexes store))]
+  (let [bucket ^org.mapdb.BTreeMap (:concept-descriptions (.indexes store))
+        all-descriptions (iterator-seq (.valueIterator ^org.mapdb.BTreeMap (.descriptions store)))]
     (doall (pmap #(.put bucket
                         (long-array [(:conceptId %) (:id %)]) %)
-                 (iterator-seq (.valueIterator ^org.mapdb.BTreeMap (.descriptions store)))))))
+                 all-descriptions))))
 
 (defn build-relationship-indices
   "Build the relationship indices.
-   Populating the child and parent relationship indices."
+   Populates the child and parent relationship indices."
   [^MapDBStore store]
   (let [all-relationships (iterator-seq (.valueIterator ^org.mapdb.BTreeMap (.relationships store)))
         child-bucket (:concept-child-relationships (.indexes store))
@@ -97,6 +104,23 @@
                                        (long-array [(:destinationId %) (:typeId %) (:sourceId %)])
                                        active?))
                  all-relationships))))
+
+(defn build-refset-index
+  [^MapDBStore store]
+  (let [bucket ^org.mapdb.BTreeMap (:component-refsets (.indexes store))
+        reverse-map ^org.mapdb.BTreeMap (:map-target-component (.indexes store))
+        all-refsets (iterator-seq (.valueIterator ^org.mapdb.BTreeMap (.refsets store)))]
+    (dorun (pmap #(do
+                    (.put bucket
+                          (to-array [(:referencedComponentId %) ;; refset index is made up of three elements
+                                     (:refsetId %)
+                                     (:id %)]) %)
+                    (when (:mapTarget %)
+                      (.put reverse-map
+                            (to-array [(:refsetId %)
+                                       (:mapTarget %)
+                                       (:id %)]) %)))
+                 all-refsets))))
 
 (defn get-concept
   [^MapDBStore store concept-id]
@@ -116,9 +140,13 @@
   [^MapDBStore store concept-id]
   (vals (.prefixSubMap ^org.mapdb.BTreeMap (:concept-descriptions (.indexes store)) (long-array [concept-id]))))
 
+(defn is-fully-specified-name?
+  [^Description d]
+  (= 900000000000003001 (:typeId d)))
+
 (defn get-fully-specified-name
   [^MapDBStore store concept-id]
-  (first (filter #(= 900000000000003001 (:typeId %)) (get-concept-descriptions store concept-id))))
+  (first (filter is-fully-specified-name? (get-concept-descriptions store concept-id))))
 
 (defn get-parent-relationships
   "Return the parent relationships of the given concept"
@@ -187,6 +215,23 @@
                      (long-array [concept-id type-id 0])
                      (long-array [concept-id (if (pos? type-id) type-id Long/MAX_VALUE) Long/MAX_VALUE])))))
 
+(defn get-component-refset-items
+  ([^MapDBStore store component-id]
+   (vals (.prefixSubMap
+           ^org.mapdb.BTreeMap (:component-refsets (.indexes store))
+           (to-array [component-id]))))
+  ([^MapDBStore store component-id refset-id]
+   (vals (.prefixSubMap
+           ^org.mapdb.BTreeMap (:component-refsets (.indexes store))
+           (to-array [component-id refset-id])))))
+
+(defn get-reverse-map
+  "Returns the reverse mapping from the reference set and mapTarget specified."
+  [^MapDBStore store refset-id s]
+  (vals (.prefixSubMap
+          ^org.mapdb.BTreeMap (:map-target-component (.indexes store))
+          (to-array [refset-id s]))))
+
 (defn- write-concepts [^MapDBStore store objects]
   (doseq [o objects]
     (write-object (.concepts store) o)))
@@ -204,16 +249,25 @@
     (write-object (.refsets store) o)))
 
 (defn get-relationship [^MapDBStore store relationship-id]
-  (.get ^org.mapdb.MapExtra (.relationships store) relationship-id))
+  (.get ^org.mapdb.BTreeMap (.relationships store) relationship-id))
 
 (defn get-refset-item [^MapDBStore store uid]
-  (.get ^org.mapdb.MapExtra (.refsets store) uid))
+  (.get ^org.mapdb.BTreeMap (.refsets store) uid))
 
 (defn get-extended-concept [^MapDBStore store concept-id]
-  (.get ^org.mapdb.MapExtra (.cache store) concept-id))
+  (.get ^org.mapdb.BTreeMap (.cache store) concept-id))
 
 (defn close [^MapDBStore store]
   (.close ^org.mapdb.DB (.db store)))
+
+(defn status
+  [^MapDBStore store]
+  {:concepts      (.size ^org.mapdb.BTreeMap (.concepts store))
+   :descriptions  (.size ^org.mapdb.BTreeMap (.descriptions store))
+   :relationships (.size ^org.mapdb.BTreeMap (.relationships store))
+   :refsets       (.size ^org.mapdb.BTreeMap (.refsets store))
+   :indices       (let [ks (keys (.indexes store))]
+                    (zipmap ks (map #(.size (get (.indexes store) %)) ks)))})
 
 (def default-opts
   {:read-only?  true
@@ -249,7 +303,15 @@
                   :component-refsets
                   (open-bucket db "c-rs"
                                (SerializerArrayTuple. (into-array Serializer [Serializer/LONG Serializer/LONG Serializer/STRING]))
-                               Serializer/JAVA)}]
+                               Serializer/JAVA)
+
+                  ;; for any map refsets, we store (refsetid--mapTarget--itemId) = refset item to
+                  ;; allow reverse mapping from, e.g. Read code to SNOMED-CT
+                  :map-target-component
+                  (open-bucket db "r-ti"
+                               (SerializerArrayTuple. (into-array Serializer [Serializer/LONG Serializer/STRING Serializer/STRING]))
+                               Serializer/JAVA)
+                  }]
 
      (->MapDBStore db concepts descriptions relationships refsets cache indexes))))
 
@@ -276,90 +338,40 @@
   (set! *warn-on-reflection* true)
   (use 'clojure.repl)
   (require '[clojure.java.javadoc :as javadoc])
-  (def db (open-store "snomed.db" {:read-only? false :skip-check? false}))
-  (close db)
-  (.close (.db db))
-
-  (do
-    (java.time.LocalDateTime/now)
-    (com.eldrix.hermes.core/import-snomed "/Users/mark/Downloads/uk_sct2cl_30.0.0_20200805000001/SnomedCT_InternationalRF2_PRODUCTION_20190731T120000Z"
-                                          "snomed.db")
-    (java.time.LocalDateTime/now))
 
   (def db (open-store "snomed.db" {:read-only? true :skip-check? false}))
-  (def all-descriptions)
-  (def all-relationships (iterator-seq (.valueIterator (.relationships db))))
+  (close db)
 
-  ;; build concept-description index
+  (com.eldrix.hermes.core/import-snomed
+    "/Users/mark/Downloads/uk_sct2cl_30.0.0_20200805000001/SnomedCT_InternationalRF2_PRODUCTION_20190731T120000Z"
+    "snomed.db")
 
-  ;; get descriptions for MS concept
-  (vals (.prefixSubMap (:concept-descriptions (.indexes db)) (long-array [24700007])))
-  (map :term (filter :active (vals (.prefixSubMap (:concept-descriptions (.indexes db)) (long-array [24700007])))))
+  (with-open [st (open-store "snomed.db" {:read-only? false :skip-check? false})]
+    (build-description-index st)
+    (build-relationship-indices st)
+    (build-refset-index st))
 
-  ;; build parentl relationships index
-  (first all-relationships)
+  (with-open [db (open-store "snomed.db" {:read-only? true :skip-check? false})]
+    (status db))
 
-  (def all-refsets (iterator-seq (.valueIterator (.refsets db))))
-  (first all-refsets)
-  (defn refsetitem->key [item]
-    (into-array :referencedComponentId :refsetId :id %
-                ))
-  ;; write an index of source->type->destination for each relationship
-  (let [bucket (:concept-parent-relationships (.indexes db))]
-    (dorun (pmap #(write-index-entry
-                    bucket
-                    (long-array [(:sourceId %) (:typeId %) (:destinationId %)])
-                    (:active %))
-                 all-relationships)))
+  (with-open [st (open-store "snomed.db" {:read-only? true :skip-check? false})]
+    (->> (get-concept-descriptions st 24700007)
+         (filter :active)
+         (remove is-fully-specified-name?)
+         (map :term)
+         (sort-by :term)))
 
-  (do (println "started: " (java.time.LocalDateTime/now))
-      (build-relationship-indices db)
-      (println "finished: " (java.time.LocalDateTime/now)))
+  (with-open [st (open-store "snomed.db" {:read-only? true :skip-check? false})]
+    (map :mapTarget (get-component-refset-items st 24700007 900000000000497000)))
 
-  ;; get all parent relationships of MS
-  (map seq (.subSet (:concept-parent-relationships (.indexes db)) (long-array [24700007 0 0]) (long-array [24700007 Long/MAX_VALUE Long/MAX_VALUE])))
-  ;; get list of all direct child concepts of MS
-  (->> (.subSet (:concept-child-relationships (.indexes db))
-                (long-array [24700007 116680003 0])
-                (long-array [24700007 116680003 Long/MAX_VALUE]))
-       (map seq)
-       (map last))
+  (with-open [st (open-store "snomed.db" {:read-only? true :skip-check? false})]
+    (map :referencedComponentId (get-reverse-map st 900000000000497000 "F20..")))
 
-  (defn get-parent-relationships
-    "Return the parent relationships of the given concept"
-    ([db concept-id] (get-parent-relationships db concept-id 0))
-    ([db concept-id type-id]
-     (map seq (.subSet (:concept-parent-relationships (.indexes db))
-                       (long-array [concept-id type-id 0])
-                       (long-array [24700007 (if (pos? type-id) type-id Long/MAX_VALUE) Long/MAX_VALUE])))))
-
-  ;; get all direct parents (IS-A) for multiple sclerosis
-  (map last (get-parent-relationships db 24700007 116680003))
-  (map last (get-child-relationships db 24700007 116680003))
-
-  (with-open [db (open-store "snomed.db" {:read-only? false})]
-    (.compact (.getStore (.concepts db))))
+  (with-open [st (open-store "snomed.db" {:read-only? false :skip-check? true})]
+    (.compact (.getStore (.concepts st))))
 
   (with-open [db (open-store "snomed.db" {:read-only? false})]
     (.getAllNames (.db db)))
-
-  (with-open [db (open-store "snomed.db" {:read-only? false})]
-    (println "store cd" (.getStore (:concept-descriptions (.indexes db))))
-    (println "store c" (.getStore (.concepts db))))
-
-  (do
-    (def result (with-open [db (open-store "snomed.db" {:read-only? false})]
-                  (let [test-set (.createOrOpen (.treeSet (.db db) "test3" Serializer/LONG_ARRAY))]
-                    (.add test-set (long-array [1 2 3]))
-                    (.add test-set (long-array [1 2 4]))
-                    (.add test-set (long-array [1 3 1]))
-                    (.contains test-set (long-array [1 2 3]))
-                    (seq (.subSet test-set (long-array [1 2]) (long-array [1 2 Long/MAX_VALUE]))))))
-    (map seq result))
-
-
-  (close db)
-  (.close (.db db))
 
   )
 
