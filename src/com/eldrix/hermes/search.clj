1c645c26f16bb28bf82264ab75ac383a1b819d56
Mark Wardle <mark@wardle.org>
2020-11-13 18:19:09 +0000

    First experiment with lucene from clojure.

---
+++ src/com/eldrix/hermes/search.clj
@@ -0,0 +1,28 @@
+(ns com.eldrix.hermes.search
+  (:import (java.nio.file Files)
+           (org.apache.lucene.index IndexWriter IndexWriterConfig DirectoryReader)
+           (org.apache.lucene.store FSDirectory)
+           (org.apache.lucene.document Document Field TextField)
+           (java.nio.file.attribute FileAttribute)
+           (org.apache.lucene.search IndexSearcher)
+           (org.apache.lucene.queryparser.classic QueryParser)))
+
+(comment
+  (def analyzer (org.apache.lucene.analysis.standard.StandardAnalyzer.))
+  (def path (Files/createTempDirectory "tempIndex" (into-array FileAttribute [])))
+  (def directory (FSDirectory/open path))
+  (def writer-config (IndexWriterConfig. analyzer))
+  (def writer (IndexWriter. directory writer-config ))
+  (def doc (Document.))
+  (.add doc (Field. "fieldName" "this is the text to be indexed" TextField/TYPE_STORED))
+  (.addDocument writer doc)
+  (.close writer)
+
+  (def reader (DirectoryReader/open directory))
+  (def searcher (IndexSearcher. reader))
+  (def parser (QueryParser. "fieldName" analyzer))
+  (def query (.parse parser "text"))
+  (def hits (.-scoreDocs (.search searcher query 10)))
+  (map #(.doc searcher %) (map #(.-doc %) (seq hits)))
+  (map #(.get % "fieldName") (map #(.doc searcher %) (map #(.-doc %) (seq hits))))
+  )
