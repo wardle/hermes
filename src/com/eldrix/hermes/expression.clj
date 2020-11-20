792f8add539bbddee72ceb6dd3bb9bfba3c34de0
Mark Wardle <mark@wardle.org>
2020-11-20 14:47:30 +0000

    Add cg expression rendering and round-tripping.

--- src/com/eldrix/hermes/expression.clj
+++ src/com/eldrix/hermes/expression.clj
@@ -1,8 +1,11 @@
 (ns com.eldrix.hermes.expression
   (:require [clojure.java.io :as io]
+            [clojure.string :as str]
             [instaparse.core :as insta]
             [clojure.data.zip.xml :as zx]
-            [clojure.zip :as zip]))
+            [clojure.walk :as walk]
+            [clojure.zip :as zip]
+            [com.eldrix.hermes.store :as store]))
 
 (def cg-parser
   (insta/parser (io/resource "cg-v2.4.abnf") :input-format :abnf :output-format :enlive))
@@ -10,33 +13,29 @@
 (def ecl-parser
   (insta/parser (io/resource "ecl.abnf") :input-format :abnf :output-format :enlive))
 
-(defn parse-sctId [sctId]
+(defn- parse-sctId [sctId]
   (clojure.edn/read-string (zx/xml1-> sctId zx/text)))
 
-(defn parse-conceptId [conceptId]
-  {:sctId (zx/xml1-> conceptId :sctId parse-sctId)})
-(defn parse-conceptId [conceptId]
+(defn- parse-conceptId [conceptId]
   (zx/xml1-> conceptId :sctId parse-sctId))
 
-(defn parse-concept-reference [cr]
+(defn- parse-concept-reference [cr]
   (let [conceptId (zx/xml1-> cr :conceptId parse-conceptId)
         term (zx/xml1-> cr :term zx/text)]
     (merge {:conceptId conceptId}
            (when term {:term term}))))
 
-(defn parse-focus-concept [focus-concept]
-  ;  {:conceptReferences (zx/xml-> focus-concept :conceptReference parse-concept-reference)})
+(defn- parse-focus-concept [focus-concept]
   (zx/xml-> focus-concept :conceptReference parse-concept-reference))
 
-(defn parse-attribute-name
+(defn- parse-attribute-name
   "attributeName = conceptReference"
   [attribute-name]
-  ;;  {:conceptReference (zx/xml1-> attribute-name :conceptReference parse-concept-reference)})
   (zx/xml1-> attribute-name :conceptReference parse-concept-reference))
 
 (declare parse-subexpression)
 
-(defn parse-expression-value
+(defn- parse-expression-value
   "expressionValue = conceptReference / '(' ws subExpression ws ')'"
   [expression-value]
   (let [conceptReference (zx/xml1-> expression-value :conceptReference parse-concept-reference)
@@ -44,7 +43,7 @@
     (if conceptReference conceptReference subExpression))
   )
 
-(defn parse-attribute-value
+(defn- parse-attribute-value
   "attributeValue = expressionValue / QM stringValue QM / '#' numericValue / booleanValue"
   [attribute-value]
   (let [expressionValue (zx/xml1-> attribute-value :expressionValue parse-expression-value)
@@ -55,25 +54,25 @@
       expressionValue expressionValue
       stringValue stringValue
       numericValue (clojure.edn/read-string numericValue)
-      booleanValue [(Boolean/parseBoolean booleanValue)])))      ;; hide boolean in vector so we don't break zipper
+      booleanValue [(Boolean/parseBoolean booleanValue)]))) ;; hide boolean in vector so we don't break zipper
 
-(defn parse-attribute
+(defn- parse-attribute
   "attribute = attributeName ws \"=\" ws attributeValue"
   [attribute]
   {(zx/xml1-> attribute :attributeName parse-attribute-name)
    (zx/xml1-> attribute :attributeValue parse-attribute-value)})
 
-(defn parse-attribute-set
+(defn- parse-attribute-set
   "attributeSet = attribute *(ws \",\" ws attribute)"
   [attribute-set]
   (apply merge (zx/xml-> attribute-set :attribute parse-attribute)))
 
-(defn parse-attribute-group
+(defn- parse-attribute-group
   "attributeGroup = '{' ws attributeSet ws '}'"
   [attribute-group]
   (zx/xml-> attribute-group :attributeSet parse-attribute-set))
 
-(defn parse-refinement
+(defn- parse-refinement
   "refinement = (attributeSet / attributeGroup) *( ws [\",\" ws] attributeGroup )"
   [refinement]
   (let [attributeSet (zx/xml-> refinement :attributeSet parse-attribute-set)
@@ -83,7 +82,7 @@
       (when (seq attributeGroup) attributeGroup)
       )))
 
-(defn parse-subexpression
+(defn- parse-subexpression
   "subExpression = focusConcept [ws \":\" ws refinement]"
   [subexpression]
   (let [focusConcept {:focusConcepts (zx/xml-> subexpression :focusConcept parse-focus-concept)}
@@ -91,63 +90,114 @@
     (if (seq refinements) (merge focusConcept {:refinements refinements})
                           focusConcept)))
 
-(defn parse-expression [expression]
+(defn- parse-expression [expression]
   (let [ds (zx/xml1-> expression :definitionStatus zx/text)]
     {:definitionStatus (if ds ds "===")
      :subExpression    (zx/xml1-> expression :subExpression parse-subexpression)}))
 
+(defn parse
+  "Parse a SNOMED-CT expression, as defined by the compositional grammar.
+  See https://confluence.ihtsdotools.org/display/DOCSCG/Compositional+Grammar+-+Specification+and+Guide"
+  [s] (zx/xml1-> (zip/xml-zip (cg-parser s)) :expression parse-expression))
+
+(defn- simplify-focus-concepts
+  [node]
+  (into #{} (map :conceptId (:focusConcepts node))))
+
+(defn- simplify-refinement
+  [node]
+  (if (map? node)
+    (zipmap (map :conceptId (keys node)) (map #(if (map? %) (dissoc % :term) %) (vals node)))
+    node))
+
+(defn simplify
+  "Simplify a SNOMED CT expression by removing content that does not aid computability."
+  [expression]
+  (walk/prewalk
+    (fn [node]
+      (if (map? node)
+        (cond-> node
+                (contains? node :focusConcepts) (assoc :focusConcepts (simplify-focus-concepts node))
+                (contains? node :refinements) (assoc :refinements (map simplify-refinement (:refinements node)))
+                true (dissoc :term))
+        node))
+    expression))
+
+(defn- render-concept
+  [config concept]
+  (str (:conceptId concept)
+       (when-not (:hide-terms? config)
+         (let [has-term (or (:term concept) (:getPreferredSynonym config))]
+           (str
+             (when has-term "|")
+             (if-let [f (:getPreferredSynonym config)]
+               (or (:term (f (:conceptId concept))) (:term concept))
+               (:term concept))
+             (when has-term "|"))))))
+
+(declare render-subexpression)
+
+(defn- render-value
+  [config value]
+  (if (map? value)
+    (cond
+      (get value :conceptId) (render-concept config value)
+      (get value :focusConcepts) (str "(" (render-subexpression config value) ")")
+      :else (throw (ex-info (str "** unknown value:'" value "' **") {:error "Unknown value" :value value})))
+    (if (number? value)
+      (str "#" value)
+      (str value))))
+
+(defn- render-refinement-set
+  [config refinements]
+
+  (let [k (map (partial render-concept config) (keys refinements))
+        v (map (partial render-value config) (vals refinements))]
+    (str/join "," (map (fn [k1 v1] (str/join "=" [k1 v1])) k v))))
+
+(defn- render-refinements
+  [config refinements]
+  (case (count refinements)
+    0 ""
+    1 (render-refinement-set config (first refinements))
+    (str "{" (str/join "} {" (map (partial render-refinement-set config) refinements)) "}")))
+
+(defn- render-subexpression
+  [config subexp]
+  (let [focus-concepts (str/join "+" (map (partial render-concept config) (:focusConcepts subexp)))
+        refinements (:refinements subexp)]
+    (if refinements (str focus-concepts ":" (render-refinements config refinements)) focus-concepts)))
+
+(defn render
+  [{:keys [store hide-terms? update-terms? locale-priorities] :as config} exp]
+  (let [cfg (if (and store update-terms? locale-priorities)
+              (if-let [langs (store/ordered-language-refsets-from-locale locale-priorities (store/get-installed-reference-sets store))]
+                (assoc config :getPreferredSynonym #(store/get-preferred-synonym store % langs))
+                config)
+              config)]
+    (str (:definitionStatus exp) " " (render-subexpression cfg (:subExpression exp)))))
+
+
 (comment
-  (def p (cg-parser "24700007"))
-  (def p (cg-parser "80146002|appendectomy|:260870009|priority|=25876001|emergency|, 425391005|using access device|=86174004|laparoscope|"))
-  (def p (cg-parser "421720008 |Spray dose form|  +  7946007 |Drug suspension|"))
-  (def p (cg-parser "  71388002 |Procedure| :
-       405815000 |Procedure device|  =  122456005 |Laser device| ,
-       260686004 |Method|  =  129304002 |Excision - action| ,
-       405813007 |Procedure site - direct|  =  15497006 |Ovarian structure|"))
-  (def p (cg-parser "313056006 |Epiphysis of ulna| :  272741003 |Laterality|  =  7771000 |Left|"))
-  (def p (cg-parser "   119189000 |Ulna part|  +  312845000 |Epiphysis of upper limb| :  272741003 |Laterality|  =  7771000 |Left|"))
-  (def p (cg-parser "    71388002 |Procedure| :
-   {  260686004 |Method|  =  129304002 |Excision - action| ,
-      405813007 |Procedure site - direct|  =  15497006 |Ovarian structure| }
-   {  260686004 |Method|  =  129304002 |Excision - action| ,
-      405813007 |Procedure site - direct|  =  31435000 |Fallopian tube structure| }"))
-  (def p (cg-parser "    71388002 |procedure| :
+  (def p (parse "24700007"))
+  (def st (store/open-store "snomed.db"))
+  (render {:store st :update-terms? true :locale-priorities "en-GB"} p)
+  (def p (parse "80146002|appendectomy|:260870009|priority|=25876001|emergency|, 425391005|using access device|=86174004|laparoscope|"))
+  (render {:store st :update-terms? true :locale-priorities "en-GB"} p)
+  (simplify p)
+  (def p (parse "    71388002 |procedure| :
    {  260686004 |method|  =  129304002 |excision - action| ,
       405813007 |procedure site - direct| =  20837000 |structure of right ovary| ,
       424226004 |using device|  =  122456005 |laser device| }
    {  260686004 |method|  =  261519002 |diathermy excision - action| ,
       405813007 |procedure site - direct| =  113293009 |structure of left fallopian tube| }"))
-  (def p (cg-parser "   373873005 |Pharmaceutical / biologic product| :
-       411116001 |Has dose form|  = ( 421720008 |Spray dose form|  +  7946007 |Drug suspension| )"))
-  (def p (cg-parser "    243796009 |Situation with explicit context| :
-   {  408730004 |Procedure context|  =  385658003 |Done| ,
-      408731000 |Temporal context|  =  410512000 |Current or specified| ,
-      408732007 |Subject relationship context|  =  410604004 |Subject of record| ,
-      363589002 |Associated procedure|  =
-     (  397956004 |Prosthetic arthroplasty of the hip| :
-           405814001 |Procedure site - indirect|  = ( 24136001 |Hip joint structure| :
-              272741003 |Laterality|  =  7771000 |Left| )
-          {  363699004 |Direct device|  =  304120007 |Total hip replacement prosthesis| ,
-             260686004 |Method|  =  425362007 |Surgical insertion - action| } ) }"))
-  (def p (cg-parser "  774586009 |Amoxicillin only product| :
-       411116001 |Has manufactured dose form|  =  420692007 |Oral capsule| ,
-      {  127489000 |Has active ingredient|  =  372687004 |Amoxicillin| ,
-         179999999100 |Has basis of strength|  =  372687004 |Amoxicillin| ,
-         189999999103 |Has strength value|  = #500,  199999999101 |Has strength unit|  =  258684004 |mg| }"))
-  (def p (cg-parser " 91143003 |Albuterol| :
+  (def p (parse " 91143003 |Albuterol| :
        411116001 |Has manufactured dose form|  =  385023001 |oral solution| ,
       { 127489000 |Has active ingredient|  =  372897005 |Albuterol| ,
         179999999100 |Has basis of strength|  =  372897005 |Albuterol| ,
         189999999103 |Has strength value|  = #0.083,  199999999101 |Has strength numerator unit|  =  118582008 |%| }"))
-  (def p (cg-parser "   322236009 |Paracetamol 500mg tablet| :  209999999104 |Has trade name|  = \"PANADOL\""))
-  (def p (cg-parser "318969005 |Irbesartan 150 mg oral tablet| :  859999999102 |Is in national benefit scheme|  = TRUE"))
-  (def p (cg-parser " <<<  73211009 |Diabetes mellitus| :  363698007 |Finding site|  =  113331007 |Endocrine system|"))
-  (do
-    (def root (zip/xml-zip p))
-    (zx/xml1-> root :expression parse-expression))
-  (def result (zx/xml1-> root :expression parse-expression))
 
-  (require '[clojure.data.json :as json])
-  (println (json/write-str result))
+
+  (def rs (get-in v [:subExpression :refinements]))
 
   )
