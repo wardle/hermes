35386e6e3e65d56e0e15ce7853a80eb130ed69b5
Mark Wardle <mark@wardle.org>
2020-11-30 16:19:40 +0000

    Rename service to terminology.
    In preparation for moving out implementation and working to abstractions as defined by protocols/interfaces.

--- src/com/eldrix/hermes/terminology.clj
+++ src/com/eldrix/hermes/terminology.clj
@@ -1,4 +1,6 @@
-(ns com.eldrix.hermes.service
+(ns com.eldrix.hermes.terminology
+  "Provides a terminology service, wrapping the SNOMED store and
+  search implementations as a single unified service."
   (:require
     [integrant.core :as ig]
     [com.eldrix.hermes.store :as store]
@@ -8,8 +10,9 @@
     [clojure.tools.logging :as log])
   (:import (com.eldrix.hermes.store MapDBStore)
            (java.io Closeable)
-           (org.apache.lucene.search IndexSearcher)))
-
+           (org.apache.lucene.search IndexSearcher)
+           (org.apache.lucene.index IndexReader)
+           (java.nio.file LinkOption Paths Files)))
 
 (defprotocol SnomedService
   (getConcept [_ concept-id])
@@ -17,13 +20,13 @@
   (getDescriptions [_ concept-id])
   (getReferenceSets [_ component-id])
   (getPreferredSynonym [_ concept-id langs])
-  (subsumedBy [_ concept-id subsumer-concept-id])
+  (subsumedBy? [_ concept-id subsumer-concept-id])
   (parseExpression [_ s])
   (search [_ params]))
 
 (deftype Service [^MapDBStore store
-                  ^org.apache.lucene.index.IndexReader index-reader
-                  ^org.apache.lucene.search.IndexSearcher searcher]
+                  ^IndexReader index-reader
+                  ^IndexSearcher searcher]
   Closeable
   (close [_] (.close store) (.close index-reader))
 
@@ -40,22 +43,41 @@
   (getPreferredSynonym [_ concept-id langs]
     (let [lang-refsets (store/ordered-language-refsets-from-locale langs (store/get-installed-reference-sets store))]
       (store/get-preferred-synonym store concept-id lang-refsets)))
-  (subsumedBy [_ concept-id subsumer-concept-id]
+  (subsumedBy? [_ concept-id subsumer-concept-id]
     (contains? (store/get-all-parents store concept-id) subsumer-concept-id))
   (parseExpression [_ s]
     (expression/parse s))
   (search [_ params]
     (search/do-search searcher params)))
 
-(defn ^SnomedService open-service [store-filename search-filename]
+(defn- ^SnomedService open-service [store-filename search-filename]
   (let [st (store/open-store store-filename)
         index-reader (search/open-index-reader search-filename)
         searcher (IndexSearcher. index-reader)]
     (->Service st index-reader searcher)))
 
+(def manifest
+  {:store  {:path    "store.db"
+            :version 0.1}
+   :search {:path    "search.db"
+            :version 0.1}})
+
+(defn  create-or-open-service [path]
+  (let [path (Paths/get path (into-array String []))
+        exists? (Files/exists path (into-array LinkOption []))
+        is-directory? (Files/isDirectory path (into-array LinkOption []))]
+    {:path path
+     :exists       exists?
+     :is-directory is-directory?}))
+
 (def config
-  {:com.eldrix.hermes.snomed/Store  {:filename "snomed.db"}
+  {:com.eldrix.hermes/Service       {:store ig/ref :com.eldrix.hermes.snomed/Store
+                                            :search ig/ref :com.eldrix.hermes.snomed/Search}
+   :com.eldrix.hermes.snomed/Store  {:filename "snomed.db"}
    :com.eldrix.hermes.snomed/Search {:filename "search.db"}})
+
+(defmethod ig/init-key :com.eldrix.hermes/Terminology [_ {:keys [path]}]
+  (open-service))
 
 (defmethod ig/init-key :com.eldrix.hermes.snomed/Store [_ {:keys [filename]}]
   (store/open-store filename))
@@ -64,11 +86,13 @@
   (.close store))
 
 (defmethod ig/init-key :com.eldrix.hermes.snomed/Search [_ {:keys [filename]}]
-  (search/open-index-reader filename))
+  (let [reader (search/open-index-reader filename)
+        searcher (IndexSearcher. reader)]
+    {:reader   reader
+     :searcher searcher}))
 
 (defmethod ig/halt-key! :com.eldrix.hermes.snomed/Search [_ search]
-  (.close search))
-
+  (.close (:searcher search)))
 
 (comment
   (def service (ig/init config))
@@ -79,4 +103,22 @@
   (getConcept svc 24700007)
   (search svc {"s" "multiple sclerosis" "max-hits" 1})
   (.close svc)
+  )
+
+
+(comment
+
+  (def parents #{138875005
+                 21483005
+                 442083009
+                 123037004
+                 25087005
+                 91689009
+                 91723000})
+  (def st (store/open-store "snomed.db"))
+
+  ;; for each parent, get all of the parents, ensuring each combination of parents is itself unique and non-empty
+  (def parents (disj (store/get-all-parents st 95883001) 95883001))
+
+  (def unique-parents (filter seq (into #{} (map #(disj (store/get-all-parents st %) %) parents))))
   )
