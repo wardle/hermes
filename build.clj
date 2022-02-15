4de309c6817f3ce849537fee8f307964f28a937a
Mark Wardle <mark@wardle.org>
2022-02-15 21:20:57 +0000

    Bump to v0.9 series

--- build.clj
+++ build.clj
@@ -3,7 +3,7 @@
             [deps-deploy.deps-deploy :as dd]))
 
 (def lib 'com.eldrix/hermes)
-(def version (format "0.8.%s" (b/git-count-revs nil)))
+(def version (format "0.9.%s" (b/git-count-revs nil)))
 (def class-dir "target/classes")
 (def jar-basis (b/create-basis {:project "deps.edn"}))
 (def uber-basis (b/create-basis {:project "deps.edn"
