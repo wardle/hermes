39674b13dcb1addc5dc2f1819f40409ea6da8116
Mark Wardle <mark@wardle.org>
2022-05-17 13:28:31 +0100

    Improve download provider parameters formatting
    For backwards compatibility, parameters can still be provided as alternating keys and values on the command-line, but now also support parameters of the form 'key=value'.

--- src/com/eldrix/hermes/download.clj
+++ src/com/eldrix/hermes/download.clj
@@ -34,7 +34,25 @@
    "uk.nhs/sct-drug-ext" {:f    (fn [params] (:archiveFilePath (download-from-trud 105 params)))
                           :spec ::uk-trud}})
 
-(s/def ::provider-parameters (s/* (s/cat ::key string? ::value string?)))
+(s/def ::provider-parameters (s/or :items (s/coll-of #(re-matches #".*\=.*" %))
+                                   :pairs (s/* (s/cat ::key string? ::value string?))))
+
+(defn parse-provider-parameters
+  "Parse a sequence of string arguments returning a map of keys to values.
+  Arguments can be provided as alternating key value pairs, or as strings of
+  the format key = value.
+
+  The following are equivalent:
+  ```
+  (parse-provider-parameters [\"api-key\" \"../trud/api-key.txt\"])
+  (parse-provider-parameters [\"api-key=../trud/api-key.txt\"])
+   => {:api-key \"../trud/api-key.txt\"}
+  ```"
+  [args]
+  (let [[mode params] (s/conform ::provider-parameters args)]
+    (->> (case mode :pairs (reduce (fn [acc {::keys [key value]}] (assoc acc key value )) {} params)
+                    :items (apply hash-map (mapcat #(str/split % #"=") params)))
+         (walk/keywordize-keys))))
 
 (defn print-providers
   "Placeholder for a more sophisticated future method of printing available
@@ -46,20 +64,21 @@
   "Download the named distribution.
   Parameters:
   - nm         : name of the provider   e.g. \"uk.nhs/sct-clinical\"
-  - parameters : a sequence of key value pairs with optional parameters.
+  - parameters : a sequence of strings representing alternating key value pairs,
+                 or strings of the format key=value
 
   The parameters will depend on the exact nature of the provider.
   Returns the java.nio.file.Path of the directory containing unzipped files."
   ^Path [nm parameters]
   (if-not (s/valid? ::provider-parameters parameters)
-    (println "Parameters must be given as key value pairs. e.g. \"api-key key.txt" \" (expound/expound-str ::provider-parameters parameters))
+    (println "Parameters must be given as key value pairs. e.g. \"api-key key.txt\" or \"api-key=key.txt\"" (expound/expound-str ::provider-parameters parameters))
     (let [{:keys [f spec]} (get registry nm)]
       (when-not f
         (throw (IllegalArgumentException. (str "Unknown provider: " nm))))
-      (let [params (walk/keywordize-keys (apply hash-map parameters))]
+      (let [params (parse-provider-parameters parameters)]
         (println "Downloading using provider " nm " with params" params)
         (if-not (and spec (s/valid? spec params))
-          (println "Invalid parameters for provider '" nm "':" (expound/expound-str spec params {:print-specs? false :theme :figwheel-theme}))
+          (println "Invalid parameters for provider '" nm "':\n" (expound/expound-str spec params {:print-specs? false :theme :figwheel-theme}))
           (when-let [zipfile (f params)]
             (zip/unzip zipfile)))))))
 
@@ -67,6 +86,14 @@
 
   (download "uk.nhs/sct-clinical" ["api-key" "/Users/mark/Dev/trud/api-key.txt" "cache-dir" "/tmp/trud"])
   (download "uk.nhs/sct-drug-ext" ["api-key" "/Users/mark/Dev/trud/api-key.txt" "cache-dir" "/tmp/trud"])
-  (download "uk.nhs/sct-clinical" []))
+  (download "uk.nhs/sct-clinical" [])
+
+
+  (def params ["api-key" "/Users/mark/Dev/trud/api-key.txt" "cache-dir" "/tmp/trud"])
+  (def params2 ["api-key" "api-key.txt" "cache-dir=../trud/cache"])
+  (apply hash-map params)
+
+  (mapcat #(str/split %)))
+
 
 
